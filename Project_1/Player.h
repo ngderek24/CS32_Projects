d2b32923f8685c92844b32465e1fe23cb733dc8f
tincan24 <tincan24@gmail.com>
2014-09-13 12:43:41 -0700

    Create Player.h

---
+++ Project_1/Player.h
@@ -0,0 +1,30 @@
+#ifndef Player_INCLUDED
+#define Player_INCLUDED
+
+#include <string>
+class Arena;
+
+class Player
+{
+  public:
+      // Constructor
+    Player(Arena *ap, int r, int c);
+
+      // Accessors
+    int  row() const;
+    int  col() const;
+    bool isDead() const;
+
+      // Mutators
+    std::string dropBrain();
+    std::string move(int dir);
+    void   setDead();
+
+  private:
+    Arena* m_arena;
+    int    m_row;
+    int    m_col;
+    bool   m_dead;
+};
+
+#endif
